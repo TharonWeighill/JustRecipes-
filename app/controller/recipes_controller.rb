22f4cd5e81bec5a10a131d19bead1dd96adc4b62
tharon <tharonweighill@gmail.com>
2021-02-08 15:42:35 -0700

    created new.erb files in views folders

--- app/controller/recipes_controller.rb
+++ app/controller/recipes_controller.rb
@@ -4,9 +4,14 @@ class Recipes_Controller < ApplicationController
         erb :"recipes/index" 
     end 
 
+
+    get '/recipes/new' do
+        
+    end
+
     post '/recipes' do
        @recipe = Recipe.create(recipe_name: params["recipe_name"], content: params["directions"])
-        redirect "/recipes/#{recipe.id}
+        redirect "/recipes/"
     end
 
 
