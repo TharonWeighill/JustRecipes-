152c938a53c95929feacd62a1e773df41574ec3a
tharon <tharonweighill@gmail.com>
2021-02-10 16:50:23 -0700

    created .env

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= 'development'
 require 'bundler/setup'
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'dotenv/load'
 
 configure :development do
     set :database, 'sqlite3:db/database.db'
