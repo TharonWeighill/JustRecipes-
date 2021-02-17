1830d10ba6230c38f6de9980b8541ee058b5ed49
tharon <tharonweighill@gmail.com>
2021-02-17 16:51:13 -0700

    errors

--- app/controller/users_controller.rb
+++ app/controller/users_controller.rb
@@ -1,19 +1,14 @@
 class Users_Controller < ApplicationController
   set :views, Proc.new { File.join(root, "../views/users") }
   
-  #route to all users 
-  get '/users' do
-    @users = User.all
-    erb :index
-  end
   
   #route to create user / has a form to create new user
-  get '/users/new' do
-    erb :new
+  get '/signup' do
+    erb :signup
   end
   
   #create user
-  post '/users' do
+  post '/signup' do
     user = User.create(params["username"])
     redirect "/users/#{user.id}"
   end
@@ -24,7 +19,12 @@ class Users_Controller < ApplicationController
     erb :show
   end
   
-
+  #route to all users 
+  get '/users' do
+    @users = User.all
+    erb :index
+  end
+  
   #renders form to edit user
   get '/users/:id/edit' do
     @user = User.find_by(id: params[:id])
