71559f071ae7143366a7e3f3f857db0e287be76c
tharon <tharonweighill@gmail.com>
2021-02-25 12:10:13 -0700

    fixed user update

--- app/controller/recipes_controller.rb
+++ app/controller/recipes_controller.rb
@@ -1,12 +1,12 @@
- class Recipes_Controller < ApplicationController
+class Recipes_Controller < ApplicationController
   set :views, Proc.new { File.join(root, "../views/recipes") }
   
 
-#route to all recipes
-get '/recipes' do
-  @recipes = Recipe.all
-  erb :index
-end
+  #route to all recipes
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
   
   #route to create recipe / has a form to create new recipe
   get '/recipes/new' do
@@ -26,34 +26,38 @@ end
   post '/recipes' do
     redirect_if_not_logged_in
     recipe = current_user.recipes.build(params["recipe"])
-      if recipe.save
-        params["ingredients"].each do |hash|
+    if recipe.save
+      params["ingredients"].each do |hash|
       if hash["name"] != ""
-          ingredient= Ingredient.find_or_create_by(value: hash["name"].capitalize)
-          IngredientRecipe.create(ingredient: ingredient, recipe: recipe, value: hash["value"])
-        end
+        ingredient= Ingredient.find_or_create_by(value: hash["name"].capitalize)
+        IngredientRecipe.create(ingredient: ingredient, recipe: recipe, value: hash["value"])
       end
-      redirect "/recipes/#{recipe.id}"
+    end
+    redirect "/recipes/#{recipe.id}"
     else 
       flash[:error] = recipe.errors.full_messages.to_sentence
       redirect "/recipes/new"
     end 
-end
+  end
   
 
   #renders form to edit recipe
   get '/recipes/:id/edit' do
-    # redirect_if_not_logged_in
+    redirect_if_not_logged_in
     @recipe = Recipe.find_by(id: params[:id])
+    @ingredients = @recipe.ingredients
     erb :edit
   end 
   
-  #update recipe
   patch '/recipes/:id/edit' do
     recipe = Recipe.find_by(id: params[:id])
-    recipe.update(params["recipe_name"])
+    recipe.update(params["recipe"])
     redirect "/recipes/#{recipe.id}"
-    end 
+  end 
+  
+  
+
+
     
   #delete existing user
   delete '/recipes/:id' do
@@ -62,14 +66,4 @@ end
     redirect "/recipes"
   end
     
-end 
-  
-  
-  
-
-    
-    
-      
-    
-    
-  
+end
